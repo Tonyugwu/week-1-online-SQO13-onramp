7226ed502492269a83e893358b6cab02c1d0aa32
Tonyugwu <112875968+Tonyugwu@users.noreply.github.com>
2022-09-13 03:11:58 -0700

    Javascript done

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,6 +1,20 @@
 function fizzBuzz(input){
+        let fizz = input % 3 == 0;
+        let buzz = input % 5 == 0;
+        let fizzbuzz = fizz && buzz;
+        if(fizzbuzz){
+            return "fizzBuzz"
+        }
+        else if (fizz){
+            return "fizz"
+        }
+        else if(buzz){
+            return 'buzz'
+        }
 
-  
+        else if(!fizzbuzz){
+            return input
+        }
 }
 
 
